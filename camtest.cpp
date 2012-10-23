704af5acc6f8d6897e8fe0868bd628d61179c112
Paweł Lampe <Scony@scony.netbook>
2012-10-23 12:15:23 +0200

    some contours

--- camtest.cpp
+++ camtest.cpp
@@ -5,38 +5,63 @@
 
 using namespace std;
 
-int main(int argc, const char* argv[])
+int main()
 { 
   CvCapture* capture = cvCaptureFromCAM(CV_CAP_ANY);
 
-  IplImage* frame;
-  IplImage *dst;
+  IplImage * frame;
+  IplImage * dst;
+  IplImage * cpy;
+  int zm = 153;
 
-  cvNamedWindow("afterEffects", CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("raw",CV_WINDOW_AUTOSIZE);
+  cvCreateTrackbar("somee","raw",&zm,256,NULL);
+  cvNamedWindow("fixed",CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("cpy",CV_WINDOW_AUTOSIZE);
 
-  while (1)
+  while(1)
     {
 
       frame = cvQueryFrame(capture);
 
       dst = cvCreateImage( cvSize( frame->width, frame->height ), IPL_DEPTH_8U, 1 );
+      cpy = cvCreateImage( cvSize( frame->width, frame->height ), frame->depth, frame->nChannels );
+      cvCopy(frame,cpy);
       cvCvtColor(frame, dst, CV_RGB2GRAY);
 
+      cvSmooth(dst, dst, CV_GAUSSIAN, 11, 11, 2, 2);
+      cvCanny(dst, dst, 10, 60, 3);
+
+      CvMemStorage * storage = cvCreateMemStorage(0);
+      CvSeq * contour = 0;
+      int i = 153;
+      cvFindContours(dst, storage, &contour, sizeof(CvContour),CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE); // odszukac drzewo
+      for(;contour != 0; contour = contour->h_next)
+	{
+	  // cvDrawContours(dst,contour,CV_RGB(255,153,255),CV_RGB(255,153,255),CV_FILLED);
+	  cvDrawContours(cpy,contour,CV_RGB(255,i,0),CV_RGB(255,i,0),CV_FILLED);
+	  i += 20;
+	  i = i > 256 ? 256 : i;
+	}
       // cvErode(frame,frame,NULL,10);
-      cvThreshold(dst, dst, 100, 256, CV_THRESH_BINARY); // 70 x 150
+      //cvThreshold(dst, dst, 100, 256, CV_THRESH_BINARY); // 70 x 150
       // cvDilate(frame,frame,NULL,10);
 
-      cvShowImage("afterEffects", dst);
+      cvShowImage("raw", frame);
+      cvShowImage("fixed", dst);
+      cvShowImage("cpy", cpy);
 
-      if ((cvWaitKey(10) & 255) == 27)
-	{
-	  break;
-	}
+      if((cvWaitKey(10) & 255) == 27)
+	break;
     }
 
-  cvReleaseCapture(&capture);
-  cvDestroyWindow("afrerEffects");
+  cvDestroyWindow("raw");
+  cvDestroyWindow("fixed");
+  cvDestroyWindow("cpy");
   cvReleaseImage(&frame);
+  cvReleaseImage(&dst);
+  cvReleaseImage(&cpy);
+  cvReleaseCapture(&capture);
 
   return 0;
 }
