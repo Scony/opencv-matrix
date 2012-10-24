e93114f5f226e47f5831c76c90556c857861d68c
Paweł Lampe <Scony@scony.netbook>
2012-10-24 18:38:09 +0200

    contourDance

--- main.cpp
+++ main.cpp
@@ -13,15 +13,17 @@ IplImage * setUp(IplImage * in)
 
   cvCvtColor(in, gray, CV_RGB2GRAY);
 
-  cvSmooth(grayd4, grayd4, CV_GAUSSIAN, 11, 11, 2, 2);
+  // cvErode(gray,gray,NULL,4);
+  // cvDilate(gray,gray,NULL,4);
+  cvSmooth(gray, gray, CV_GAUSSIAN, 11, 11, 2, 2); //?
   cvPyrDown(gray,grayd2);
-  cvPyrDown(grayd2,grayd4);
-  cvCanny(grayd4, grayd4, 10, 60, 3);
+  // cvPyrDown(grayd2,grayd4);
+  cvCanny(grayd2, grayd2, 10, 60, 3);
 
   cvReleaseImage(&gray);
-  cvReleaseImage(&grayd2);
+  cvReleaseImage(&grayd4);
 
-  return grayd4;
+  return grayd2;
 }
 
 IplImage * getROI(IplImage * in)
@@ -41,11 +43,39 @@ IplImage * getROI(IplImage * in)
     }
 
   cvSetImageROI(in,maxBound);
-  roi = cvCloneImage(in);
+  roi = cvCreateImage(cvGetSize(in),in->depth,in->nChannels);
+  cvCopy(in,roi,NULL);
   cvResetImageROI(in);
   cvReleaseImage(&inCpy);
 
   return roi;
+}
+
+void contourDance(IplImage * in)
+{
+  IplImage * inCpy = cvCloneImage(in);
+  IplImage * colored = cvCreateImage(cvSize(inCpy->width,inCpy->height),8,3);
+
+  CvMemStorage * storage = cvCreateMemStorage(0);
+  CvSeq * firstContour = 0;
+  cvFindContours(inCpy, storage, &firstContour, sizeof(CvContour),CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+  bool exit = false;
+  while(!exit)
+    {
+      cvSet(colored,cvScalar(0,0,0));
+      for(CvSeq * contour = firstContour; contour != 0; contour = contour->h_next)
+	{
+	  cvDrawContours(colored,contour,CV_RGB(255,153,0),CV_RGB(255,153,0),CV_FILLED);
+	  cvShowImage("dance",colored);
+	  if((cvWaitKey(1000) & 255) == 27)
+	    {
+	      exit = true;
+	      break;
+	    }
+	}
+    }
+
+  cvDestroyWindow("dance");
 }
 
 int main()
@@ -53,7 +83,7 @@ int main()
   IplImage * img = cvLoadImage("frame.jpg",1);
   IplImage * rdy = setUp(img);
   IplImage * roi = getROI(rdy);
-  
+
   cvNamedWindow("src",CV_WINDOW_AUTOSIZE);
   cvNamedWindow("fix1",CV_WINDOW_AUTOSIZE);
 
@@ -61,6 +91,7 @@ int main()
   cvShowImage("fix1", roi);
 
   cvWaitKey(0);
+  contourDance(roi);
 
   cvDestroyWindow("src");
 
