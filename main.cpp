e1a565637830a94f6d4f23d111a5ba0c914ac99b
Paweł Lampe <Scony@scony.netbook>
2012-10-25 23:01:17 +0200

    filter + some resolving

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ IplImage * getROI(IplImage * in)
   for(;contour != 0; contour = contour->h_next)
     {
       CvRect bound = cvBoundingRect(contour, 1);
-      if((bound.width-bound.x)*(bound.height-bound.y) > (maxBound.width-maxBound.x)*(maxBound.height-maxBound.y))
+      if(bound.width*bound.height > maxBound.width*maxBound.height)
 	maxBound = bound;
     }
 
@@ -49,6 +49,60 @@ IplImage * getROI(IplImage * in)
   cvReleaseImage(&inCpy);
 
   return roi;
+}
+
+IplImage * filterContours(IplImage * in)
+{
+  IplImage * inCpy = cvCloneImage(in);
+  IplImage * out = cvCreateImage( cvSize( in->width, in->height ), IPL_DEPTH_8U, 1 );
+
+  CvMemStorage * storage = cvCreateMemStorage(0);
+  CvSeq * contour = 0;
+  cvFindContours(inCpy, storage, &contour, sizeof(CvContour),CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+  for(; contour != 0; contour = contour->h_next)
+    {
+      CvRect bound = cvBoundingRect(contour, 1);
+      if(bound.width > out->width*0.8 || bound.height > out->height*0.8)
+	continue;
+      if(bound.width < out->width*0.8 && bound.height < out->height*0.8 &&
+	 bound.width > 10 && bound.height > 10)
+	cvDrawContours(out,contour,CV_RGB(255,255,255),CV_RGB(255,255,255),CV_FILLED);
+    }
+
+  cvReleaseImage(&inCpy);
+
+  return out;
+}
+
+int ** resolveMatrix(IplImage * in) //wskazniki na rozmiary
+{
+  IplImage * inCpy = cvCloneImage(in);
+
+  CvMemStorage * storage = cvCreateMemStorage(0);
+  CvSeq * first = 0;
+  CvSeq * contour = 0;
+  int maxWidth = 0;
+  int maxHeight = 0;
+  cvFindContours(inCpy, storage, &first, sizeof(CvContour),CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
+  for(contour = first;  contour != 0; contour = contour->h_next)
+    {
+      CvRect bound = cvBoundingRect(contour, 1);
+      if(bound.width > maxWidth)
+	maxWidth = bound.width;
+      if(bound.height > maxHeight)
+	maxHeight = bound.height;
+      cvRectangle(in,cvPoint(bound.x,bound.y),cvPoint(bound.x+bound.width,bound.y+bound.height),CV_RGB(255,255,255),1);
+    }
+
+  //znajdz pasy (pas = klasa/struktura) i zrob ich liste
+  //do pasow wrzucaj cyfry (cyfry = klasa/struktura [->contour + rect])
+  //dla kazdego pasa przesortuj cyfry po x-ie
+  //dla kazdego pasa przerob cyfry w liczby na podstawie maxW... i maxH... (liczby = klasa/struktura)
+  //dla kazdej liczby rozwiaz jej wartosc (jak nie mozesz to -1 cala)
+
+  cvReleaseImage(&inCpy);
+
+  return NULL;
 }
 
 void contourDance(IplImage * in)
@@ -67,6 +121,7 @@ void contourDance(IplImage * in)
 	{
 	  cvDrawContours(colored,contour,CV_RGB(255,153,0),CV_RGB(255,153,0),CV_FILLED);
 	  cvShowImage("dance",colored);
+	  //show image & huMoment (store huMoment)
 	  if((cvWaitKey(1000) & 255) == 27)
 	    {
 	      exit = true;
@@ -83,21 +138,28 @@ int main()
   IplImage * img = cvLoadImage("frame.jpg",1);
   IplImage * rdy = setUp(img);
   IplImage * roi = getROI(rdy);
+  IplImage * filtered = filterContours(roi);
+  // resolveMatrix(filtered);
 
   cvNamedWindow("src",CV_WINDOW_AUTOSIZE);
   cvNamedWindow("fix1",CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("fix2",CV_WINDOW_AUTOSIZE);
 
   cvShowImage("src", rdy);
   cvShowImage("fix1", roi);
+  cvShowImage("fix2", filtered);
 
   cvWaitKey(0);
-  contourDance(roi);
+  contourDance(filtered);
 
   cvDestroyWindow("src");
+  cvDestroyWindow("fix1");
+  cvDestroyWindow("fix2");
 
   cvReleaseImage(&img);
   cvReleaseImage(&rdy);
   cvReleaseImage(&roi);
+  cvReleaseImage(&filtered);
 
   return 0;
 }
